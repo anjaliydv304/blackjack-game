fadc5ee2e74809d3e43f8605cba1635a32adb082
anjaliydv304 <anjali.ydv304@gmail.com>
2024-04-19 15:37:48 +0530

    completed

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+let player={
+    name:"Xyz",
+    chip:130
+}
 let cards = []
 let sum =0
 let hasBlackJack = false
@@ -7,8 +10,9 @@ let message=""
 let messageEl=document.getElementById("message-el")
 let sumEl=document.getElementById("sum-el")
 let cardEl=document.getElementById("cards-el")
+let playerEl=document.getElementById("player-el")
 
-
+playerEl.textContent=player.name+":$"+player.chip
 function getRandomCard(){
     let randomNumber = Math.floor(Math.random()*13)+1
 
